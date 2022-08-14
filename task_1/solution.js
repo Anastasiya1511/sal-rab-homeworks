819ce742ae63f71ba7da16bb93eb7c767b7f99db
Galeeva Anastasiya <agao.an@yandex.ru>
2022-08-14 13:58:35 +0200

    task_1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -10,38 +10,22 @@ function addInCartMessage(title, price) {
 
     console.log(message);
 
-    // выведите переменную message в консоль
-
-    // Конец решения задания №1.1.
-
     return message;
 }
-
 function addInCartCountChange(value) {
 
-    // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
-
-    // Конец решения задания №1.2.
+    let newValue = oldValue; 
+    console.log (newValue+1);
 
     return newValue;
 }
-
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
-
-    // Задание №1.3. Увеличение суммы в корзине
-
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
-
-    // Конец решения задания №1.3.
+    let newSum = oldSum;
+    let newSum = difference;
+    let newSumText = `${newSum} ₽`
 
     return newSumText;
 }
