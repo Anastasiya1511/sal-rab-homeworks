4beece581bbe5583e496aaf7b6ea4ab5455d5c8e
Galeeva Anastasiya <agao.an@yandex.ru>
2022-08-14 02:43:33 +0200

    task1 ok

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,17 +1,15 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message = productName;
+    message = message + " ";
+    message = message "за";
+   let message = productPrice;
+    message = message " ";
+    message = message "теперь в корзине!";
 
-    // Задание №1.1. Оповещение о добавлении в корзину
+    console.log(message);
 
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
 
     // Конец решения задания №1.1.
