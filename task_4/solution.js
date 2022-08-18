b2de25711943973e0c69205491afb929ff926d2a
Galeeva Anastasiya <agao.an@yandex.ru>
2022-08-18 21:13:34 +0200

    done

--- task_4/solution.js
+++ task_4/solution.js
@@ -4,17 +4,15 @@ function parseProducts(json) {
     let data = JSON.parse(json);
     let products = data.products;
     return products;
-    }
+    
     function renderProductsCards(json) {
       clearProducts();
-      render(let products);
-      products = parseProducts(json);
-      length = products.length;
-    for (let i = 0; i < length; i +=1) {
-      addProduct(products[i]);
-    
-    }
-    
-    }
+     let products = parseProducts(json);
+     let length = products.length;
+        for (let i = 0; i < length; i +=1) {
+            addProduct(products[i]);
+        }
+
+      }
 
     }
